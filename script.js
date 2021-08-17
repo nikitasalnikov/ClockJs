a0f804b19af8f2208bca8002a94ad6366115aecb
nikitasalnikov <nikitasalnikov@yandex.ru>
2021-08-17 16:30:14 +0500

    clockJS

---
+++ script.js
@@ -0,0 +1,82 @@
+let num = prompt('Введите время: ');
+
+num = Number(num);
+
+switch (num) {
+    case 0:
+        alert('12 часов ночи!');
+        break;
+    case 1:
+        alert('1 час ночи!');
+        break;
+    case 2:
+        alert('2 часа ночи!');
+        break;
+    case 3:
+        alert('3 часа ночи!');
+        break;
+    case 4:
+        alert('4 часов ночи!');
+        break;
+    case 5:
+        alert('5 часов утра!');
+        break;
+    case 6:
+        alert('6 часов утра!');
+        break;
+    case 7:
+        alert('7 часов утра!');
+        break;
+    case 8:
+        alert('8 часов утра!');
+        break;
+    case 9:
+        alert('9 часов утра!');
+        break;
+    case 10:
+        alert('10 часов утра!');
+        break;
+    case 11:
+        alert('11 часов дня!');
+        break;
+    case 12:
+        alert('12 часов дня!');
+        break;
+    case 13:
+        alert('13 часов дня!');
+        break;
+    case 14:
+        alert('14 часов дня!');
+        break;
+    case 15:
+        alert('15 часов дня!');
+        break;
+    case 16:
+        alert('16 часов дня!');
+        break;
+    case 17:
+        alert('17 часов дня!');
+        break;
+    case 18:
+        alert('18 часов вечера!');
+        break;
+    case 19:
+        alert('19 часов вечера!');
+        break;
+    case 20:
+        alert('20 часов вечера!');
+        break;
+    case 21:
+        alert('21 час вечера!');
+        break;
+    case 22:
+        alert('22 часа ночи!');
+        break;
+    case 23:
+        alert('23 часа ночи!');
+        break;
+
+    default:
+        alert('Неверный формат даты!');
+        break;
+}
